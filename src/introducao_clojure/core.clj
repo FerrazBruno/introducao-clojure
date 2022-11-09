4fa48a273c34bda8aa73271f22020451303ca325
Bruno Ferraz <brun00b@pop-os.localdomain>
2022-11-09 09:57:25 -0300

    Capitulo 8

--- src/introducao_clojure/core.clj
+++ src/introducao_clojure/core.clj
@@ -350,3 +350,188 @@
 
 ;; Capitulo 7
 ;; Pureza e imutabilidade
+
+;; 7.1 Pureza
+(def de-para [{:de "a" :para "4"}
+              {:de "e" :para "3"}
+              {:de "i" :para "1"}
+              {:de "o" :para "0"}])
+
+(defn escrita-hacker [texto dicionario]
+  (if (empty? dicionario)
+    texto
+    (let [conversao (first dicionario)]
+      (escrita-hacker (clojure.string/replace texto (:de conversao) (:para conversao)) (rest dicionario)))))
+
+(escrita-hacker "alameda" de-para)
+(clojure.string/replace "alameda" "a" "4")
+
+;; Mudancas de estado
+;;  Alterar valores dentro de um mapa
+;;  Alterar uma string
+;;  Escrever em um arquivo
+;;  Imprimir algo na tela
+;;  Inserir um registro no banco de dados
+
+(def cotacoes {:yuan {:cotacao 2.15M :simbolo "¥"}
+               :euro {:cotacao 0.28M :simbolo "€"}})
+
+(defn transacao-convertida [cotacoes moeda transacao]
+  (let [{{cotacao :cotacao simbolo :simbolo} moeda} cotacoes]
+    (assoc transacao :valor (* cotacao (:valor transacao)) :moeda simbolo)))
+
+(def transacao-em-outra-moeda (partial transacao-convertida cotacoes))
+
+(transacao-em-outra-moeda :euro (first transacoes))
+
+(defn transacao-em-outra-moeda
+  ([cotacoes moeda transacao]
+   (let [{{cotacao :cotacao simbolo :simbolo} moeda} cotacoes]
+     (assoc transacao :valor (* cotacao (:valor transacao)) :moeda simbolo)))
+  ([moeda transacao]
+   (transacao-em-outra-moeda cotacoes moeda transacao)))
+
+(transacao-em-outra-moeda :euro (first transacoes))
+
+;; 7.2 Imutabilidade
+
+(def membros-fundadores '("Argentina" "Brasil" "Paraguai" "Uruguai"))
+membros-fundadores
+
+(def membros-plenos (cons "Venezuela" membros-fundadores))
+membros-plenos
+(rest membros-plenos)
+membros-plenos
+
+(identical? (rest membros-plenos) membros-fundadores)
+
+transacoes
+
+(def registros (atom ()))
+registros
+@registros
+
+(swap! registros conj {:valor 29M :tipo "despesa" :comentario "Livro de Clojure" :moeda "R$" :data "03/12/2016"})
+@registros
+
+(swap! registros conj {:valor 2700M :tipo "receita" :comentario "Bico" :moeda "R$" :data "01/12/2016"})
+@registros
+
+(defn registrar [transacao]
+  (swap! registros conj transacao))
+
+(registrar {:valor 33M :tipo "despesa" :comentario "Almoco" :moeda "R$" :data "19/11/2016"})
+@registros
+
+(registrar {:valor 45M :tipo "despesa" :comentario "Jogo no Steam" :moeda "R$" :data "26/12/2016"})
+@registros
+
+(def transacoes @registros)
+transacoes
+
+;; 7.3 Imutabilidade no nosso dominio
+;; 7.4 Recursao
+
+(defn despesa? [transacao]
+  (= (:tipo transacao) "despesa"))
+
+(defn saldo-acumulado [acumulado transacoes]
+  (if-let [transacao (first transacoes)]
+    (saldo-acumulado (if (despesa? transacao)
+                       (- acumulado (:valor transacao))
+                       (+ acumulado (:valor transacao)))
+                     (rest transacoes))
+    acumulado))
+
+(saldo-acumulado 0 transacoes)
+(saldo-acumulado 0 ())
+(saldo-acumulado 0 (take 2 transacoes))
+
+(defn calcular [acumulado transacao]
+  (let [valor (:valor transacao)]
+    (if (despesa? transacao)
+      (- acumulado valor)
+      (+ acumulado valor))))
+
+(defn saldo-acumulado [acumulado transacoes]
+  (if-let [transacao (first transacoes)]
+    (saldo-acumulado (calcular acumulado transacao) (rest transacoes))
+    acumulado))
+
+(saldo-acumulado 0 transacoes)
+(saldo-acumulado 0 ())
+(saldo-acumulado 0 (take 2 transacoes))
+
+(defn valor-sinalizado [transacao]
+  (let [moeda (:moeda transacao)
+        valor (:valor transacao)]
+    (if (despesa? transacao)
+      (str moeda " -" valor)
+      (str moeda " +" valor))))
+
+(defn saldo-acumulado [acumulado transacoes]
+  (prn "Comecou saldo-acumulado. Saldo ate agora: " acumulado)
+  (if-let [transacao (first transacoes)]
+    (do
+      (prn "Valor da transacao atual: " (valor-sinalizado transacao))
+      (prn "Qauntidades de transacoes restantes: " (count (rest transacoes)))
+      (prn)
+      (saldo-acumulado (calcular acumulado transacao) (rest transacoes)))
+    (do
+      (prn "Processao encerrado. Saldo final: " acumulado))))
+
+(saldo-acumulado 0 transacoes)
+
+(defn saldo-acumulado [acumulado transacoes]
+  (if-let [transacao (first transacoes)]
+    (saldo-acumulado (calcular acumulado transacao) (rest transacoes))
+    acumulado))
+
+(defn saldo [transacoes]
+  (saldo-acumulado 0 transacoes))
+
+(saldo transacoes)
+
+(defn saldo
+  ([transacoes]
+   (saldo 0 transacoes))
+  ([acumulado transacoes]
+   (if-let [transacao (first transacoes)]
+     (saldo (calcular acumulado transacao) (rest transacoes))
+     acumulado)))
+
+(saldo transacoes)
+(saldo 0 transacoes)
+
+(defn como-transacao [valor]
+  {:valor valor})
+
+(def poucas-transacoes
+  (map como-transacao (range 10)))
+
+(def muitas-transacoes
+  (map como-transacao (range 1000)))
+
+(def incontaveis-transacoes
+  (map como-transacao (range 1000000)))
+
+(saldo poucas-transacoes)
+(saldo muitas-transacoes)
+;; (saldo incontaveis-transacoes) ERRO -> Execution error (StackOverflowError)
+
+(defn saldo
+  ([transacoes] (saldo 0 transacoes))
+  ([acumulado transacoes]
+   (if (empty? transacoes)
+     acumulado
+     (recur (calcular acumulado (first transacoes)) (rest transacoes)))))
+
+(saldo muitas-transacoes)
+(saldo incontaveis-transacoes)
+
+(reduce calcular 0 transacoes)
+
+;; 7.5 Conclusao
+
+;; Capitulo 8
+;; Preguica
